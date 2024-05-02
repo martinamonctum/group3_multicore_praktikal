060a374767eeb881238dd5d9cb9b9a1a019a8cb3
Lucas Duckerschein <lucas.duckerschein@tum.de>
2024-05-02 23:15:17 +0200

    changed relax jacobi to integrate red black ordering and zick zack.

--- HeatProject/src/relax_jacobi.c
+++ HeatProject/src/relax_jacobi.c
@@ -33,23 +33,57 @@ double residual_jacobi(double *u, unsigned sizex, unsigned sizey) {
 /*
  * One Jacobi iteration step
  */
-void relax_jacobi(double *u, double *utmp, unsigned sizex, unsigned sizey) {
+void relax_jacobi(double *u, double *utmp,unsigned sizex, unsigned sizey) {
+
 	int i, j;
 
-	for (j = 1; j < sizex - 1; j++) {
-		for (i = 1; i < sizey - 1; i++) {
-			utmp[i * sizex + j] = 0.25 * (u[i * sizex + (j - 1)] +  // left
+    // this only works for even resólution, for uneven resolution the loops have to be split, due to the borders.
+	for (i = 1; i < sizey - 1; i+=4) {
+	    for (j = 2; j < sizex - 1; j+=2) {
+			u[i * sizex + j] = 0.25 * (u[i * sizex + (j - 1)] +  // left
 						u[i * sizex + (j + 1)] +  // right
 						u[(i - 1) * sizex + j] +  // top
 						u[(i + 1) * sizex + j]); // bottom
+            u[(i+1) * sizex + (j-1)] = 0.25 * (u[(i+1) * sizex + (j-2)] +  // left
+						u[(i+1) * sizex + j] +  // right
+						u[i * sizex + (j-1)] +  // top
+						u[(i+2) * sizex + (j-1)]); // bottom
+		}
+        for (j = sizex-2; j > 0; j-=2) {
+			u[(i+2) * sizex + j] = 0.25 * (u[(i+2) * sizex + (j - 1)] +  // left
+						u[(i+2) * sizex + (j + 1)] +  // right
+						u[(i+1) * sizex + j] +  // top
+						u[(i+3) * sizex + j]); // bottom
+            u[(i+3) * sizex + (j-1)] = 0.25 * (u[(i+3) * sizex + (j-2)] +  // left
+						u[(i+3) * sizex + j] +  // right
+						u[(i+2) * sizex + (j-1)] +  // top
+						u[(i+4) * sizex + (j-1)]); // bottom
 		}
 	}
 
-	// copy from utmp to u
-
-	for (j = 1; j < sizex - 1; j++) {
-		for (i = 1; i < sizey - 1; i++) {
-			u[i * sizex + j] = utmp[i * sizex + j];
+    for (i = 1; i < sizey - 1; i+=4) {
+	    for (j = 1; j < sizex - 1; j+=2) {
+            u[i * sizex + j] = 0.25 * (u[i * sizex + (j - 1)] +  // left
+						u[i * sizex + (j + 1)] +  // right
+						u[(i - 1) * sizex + j] +  // top
+						u[(i + 1) * sizex + j]); // bottom
+			u[(i+1) * sizex + (j+1)] = 0.25 * (u[(i+1) * sizex + j] +  // left
+						u[(i+1) * sizex + (j + 2)] +  // right
+						u[i * sizex + (j+1)] +  // top
+						u[(i+2) * sizex + (j+1)]); // bottom
+            
+		}
+        for (j = sizex-3; j > 0; j-=2) {
+            u[(i+2) * sizex + j] = 0.25 * (u[(i+2) * sizex + (j - 1)] +  // left
+						u[(i+2) * sizex + (j + 1)] +  // right
+						u[(i+1) * sizex + j] +  // top
+						u[(i+3) * sizex + j]); // bottom
+			u[(i+3) * sizex + (j+1)] = 0.25 * (u[(i+3) * sizex + j] +  // left
+						u[(i+3) * sizex + (j + 2)] +  // right
+						u[(i+2) * sizex + (j+1)] +  // top
+						u[(i+4) * sizex + (j+1)]); // bottom
+            
 		}
 	}
+
 }
