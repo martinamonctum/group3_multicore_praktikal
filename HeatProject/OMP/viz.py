5afcb0089a0d2795173b2cdcfd220c147767eabd
Lucas Duckerschein <lucas.duckerschein@tum.de>
2024-05-10 22:25:31 +0200

    added 4.3.1

--- HeatProject/OMP/viz.py
+++ HeatProject/OMP/viz.py
@@ -3,11 +3,15 @@ import numpy as np
 import sys
 
 file1 = sys.argv[1]
+file2 = sys.argv[2]
 
 cores,rt_2000,rt_6000 = np.loadtxt(file1,comments=["#"],unpack=True)
+cores_2,rt_2000_2,rt_6000_2 = np.loadtxt(file2,comments=["#"],unpack=True)
 
 plt.plot(cores,rt_2000[0]/rt_2000, label="Resolution=2000")
 plt.plot(cores,rt_6000[0]/rt_6000, label="Resolution=6000")
+plt.plot(cores_2,rt_2000_2[0]/rt_2000_2, label="Resolution=2000 with ft")
+plt.plot(cores_2,rt_6000_2[0]/rt_6000_2, label="Resolution=6000 with ft")
 plt.ylabel("xSpeedup")
 plt.xlabel("Number of Cores")
 plt.axis([1,48,1,plt.ylim()[1]])
