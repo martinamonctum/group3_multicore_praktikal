8c2536335db01f67dbb4434c836cc269492b5b90
ahmed hesham <61334140+AhmedHesham307@users.noreply.github.com>
2024-06-22 17:13:41 +0200

    added performance check

--- abalone/search-minMax.cpp
+++ abalone/search-minMax.cpp
@@ -6,13 +6,11 @@
  * (c) 2006, Josef Weidendorfer
  */
 #include <iostream>
+#include <chrono>
 #include "search.h"
 #include "board.h"
 #include "eval.h"
 
-
-#define max(i,j) ((i>j)?i:j)
-#define min(i,j) ((i<j)?i:j)
 
 /**
  * To create your own search strategy:
@@ -45,56 +43,84 @@ class MinMaxAlgorithm: public SearchStrategy
      */
     int minimax(int depth, bool isMaximizing);
     void searchBestMove();
+    int totalEvaluations = 0;
+    std::chrono::duration<double> total_time;
 };
-
 
 void MinMaxAlgorithm::searchBestMove()
 {
-    _maxDepth = 2;
+    _maxDepth = 3; 
     int eval;
-    eval = minimax(0 ,true);
-    printf("best evaluation = %d\n" , eval);
+
+    FILE *fp;
+    fp = fopen("data.txt","a");
+    
+    total_time = std::chrono::duration<double>(0);
+
+    eval = minimax(0, true);
+
+    double evaluationsPerSecond = totalEvaluations / total_time.count();
+
+    printf("Best evaluation = %d\n", eval);
+    printf("Total evaluations = %d\n", totalEvaluations);
+    printf("Time taken for evaluations = %.2f seconds\n", total_time.count());
+    printf("Evaluations per second = %.2f\n", evaluationsPerSecond);
+    fprintf(fp, "%d %.2f\n", _maxDepth ,evaluationsPerSecond);
+
 }
 
 int MinMaxAlgorithm::minimax(int depth, bool isMaximizing)
 {
     if (depth == _maxDepth) {
-        return evaluate();
+        totalEvaluations++;
+        
+        int eval = evaluate();
+        return eval;
     }
+
     Move m;
     MoveList list;
+
     generateMoves(list);
+
     int bestEval;
     if (isMaximizing) {
+        auto start = std::chrono::steady_clock::now();
         bestEval = minEvaluation();
         int eval;
-        while(list.getNext(m)) {
+        while (list.getNext(m)) {
             playMove(m);
             eval = -minimax(depth + 1, false);
             takeBack();
-            if(eval > bestEval){
+            if (eval > bestEval) {
                 bestEval = eval;
-                foundBestMove(depth,m,bestEval);
+                foundBestMove(depth, m, bestEval);
             }
         }
-        finishedNode(depth,0);
+        finishedNode(depth, 0);
+        auto end = std::chrono::steady_clock::now();
+
+        total_time += end - start;
         return bestEval;
     } else {
+        auto start = std::chrono::steady_clock::now();
         bestEval = maxEvaluation();
         int eval;
-        while(list.getNext(m)) {
+        while (list.getNext(m)) {
             playMove(m);
             eval = minimax(depth + 1, true);
             takeBack();
-            if(eval < bestEval){
+            if (eval < bestEval) {
                 bestEval = eval;
-                foundBestMove(depth,m,bestEval);
+                foundBestMove(depth, m, bestEval);
             }
         }
-        finishedNode(depth,0);
+        finishedNode(depth, 0);
+        auto end = std::chrono::steady_clock::now();
+
+        total_time += end - start;
         return bestEval;
     }
 }
-
 // register ourselve as a search strategy
 MinMaxAlgorithm minmaxAlgorithm;
